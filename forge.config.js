8e6d73a873e19d89d85a97be0bb8e737ddc18ef0
Mark Lee <gmusicprocurator@lazymalevolence.com>
2016-12-30 11:39:40 -0800

    Fix the git revision timestamp

--- forge.config.js
+++ forge.config.js
@@ -1,12 +1,12 @@
 const child_process = require('child_process');
 
 const package = require('./package.json');
-const gitRevisionTimestamp = child_process.spawnSync('git', ['show', '--format="format:%at"', '-q']).stdout
+const gitRevisionTimestamp = child_process.spawnSync('git', ['show', '--format=%at', '-q'], {encoding: 'utf8').stdout.trim();
 
 function debianIcons() {
-  let icons = {}
+  let icons = {};
   for (size of [48, 128, 256]) {
-    icons[`${size}x${size}`] = `src/icon-${size}.png`
+    icons[`${size}x${size}`] = `src/icon-${size}.png`;
   }
 
   return icons;
