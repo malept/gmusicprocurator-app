ec76299381bde18d7f2d88090ccb82559b250cda
Mark Lee <gmusicprocurator@lazymalevolence.com>
2017-01-26 22:18:12 -0800

    Add more package/make options

--- forge.config.js
+++ forge.config.js
@@ -3,14 +3,17 @@ const child_process = require('child_process');
 const package = require('./package.json');
 const gitRevisionTimestamp = child_process.spawnSync('git', ['show', '--format=%at', '-q'], {encoding: 'utf8'}).stdout.trim();
 
-function debianIcons() {
-  let icons = {};
-  for (size of [48, 128, 256]) {
-    icons[`${size}x${size}`] = `src/icon-${size}.png`;
-  }
-
-  return icons;
+let icons = {};
+for (size of [48, 128, 256]) {
+  icons[`${size}x${size}`] = `src/icon-${size}.png`;
 }
+
+const categories = ['Audio', 'Player'];
+
+const linuxConfig = {
+  categories: categories,
+  icon: icons
+};
 
 module.exports = {
   make_targets: {
@@ -25,23 +28,25 @@ module.exports = {
     linux: [
       'deb',
       'rpm',
-      // 'flatpak',
+      'flatpak',
       'zip'
     ]
   },
   electronPackagerConfig: {
     'build-version': `${package.version}.${gitRevisionTimestamp}`,
+    icon: 'src/icon',
     ignore: ['/electron-packager']
   },
   electronWinstallerConfig: {
     name: 'GMusicProcurator'
   },
-  electronInstallerDebian: {
+  electronInstallerDebian: Object.assign({
     arch: 'any',
-    categories: ['Audio', 'Player'],
-    icon: debianIcons(),
     section: 'sound'
-  },
-  electronInstallerFlatpak: {},
-  electronInstallerRedhat: {}
+  }, linuxConfig),
+  electronInstallerFlatpak: Object.assign({
+    genericName: 'Music Player',
+    id: 'com.malept.gmusicprocurator'
+  }, linuxConfig),
+  electronInstallerRedhat: linuxConfig
 }
